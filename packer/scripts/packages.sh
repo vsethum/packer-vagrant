2842b78140f562fce512532f27448c896d58e3f0
vijay sethumadavan <vijay_sethumadavan@wsib.on.ca>
2017-11-01 10:44:19 -0400

    first commit

--- packer/scripts/packages.sh
+++ packer/scripts/packages.sh
@@ -1,26 +1,39 @@
 #!/usr/bin/env bash
 
 # add ppa to install current versions of nodejs
-apt-get install -y python-software-properties software-properties-common
-add-apt-repository -y ppa:chris-lea/node.js
-apt-get update
+#apt-get install -y python-software-properties software-properties-common
+#add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y vim git zip unzip curl wget
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
 
-apt-get install -y apache2 php5
-apt-get install -y mysql-client mysql-server
-apt-get install -y vim git zip unzip curl wget
-apt-get install -y memcached imagemagick
-apt-get install -y php5-mysql php5-memcached php5-xdebug php5-curl php5-json
 
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+#install java
+if which java >/dev/null; then
+   	echo "skip java 8 installation"
+else
+	echo "java 8 installation"
+	apt-get -y install python-software-properties software-properties-common
+    hash -r
+    add-apt-repository ppa:webupd8team/java
+    apt-get update && apt-get -y upgrade
+    echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+    apt-get -y install oracle-java8-installer
+    update-alternatives --display java
+    apt-get -y install oracle-java8-set-default && apt-get clean
+    apt-get update
+    apt-get -y install openjdk-8-jre-headless
+fi
 
-apt-get install -y nodejs
-npm install -g grunt-cli
-npm install -g bower
-npm install -g nodeunit
 
-# Change apache to run as vagrant:vagrant
-sed -i s/www-data/vagrant/ /etc/apache2/envvars
+#install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install stable
+
+
+#install node and npm
+#curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+#sudo apt-get install build-essential
